899e860e3b6ac613e02ecdcd21f1e73f51ad0c08
JIanzhu Huai <huai.3@osu.edu>
2020-02-02 13:08:45 +0800

    test angle element

--- okvis_ceres/include/msckf/ParallaxAnglePoint.hpp
+++ okvis_ceres/include/msckf/ParallaxAnglePoint.hpp
@@ -1,9 +1,9 @@
 #ifndef INCLUDE_MSCKF_PARALLAX_ANGLE_POINT_HPP_
 #define INCLUDE_MSCKF_PARALLAX_ANGLE_POINT_HPP_
 
-#include <random>
-#include <iostream>
 #include <Eigen/Geometry>
+#include <iostream>
+#include <random>
 
 namespace LWF {
 typedef Eigen::Quaterniond QPD;
@@ -14,22 +14,23 @@ typedef Eigen::MatrixXd MXD;
  * \param   vec 3x1-matrix (column vector)
  * \return skew   3x3-matrix
  */
-template<typename PrimType_>
-inline static Eigen::Matrix<PrimType_, 3, 3> skewVector(const Eigen::Matrix<PrimType_, 3, 1>& vec) {
+template <typename PrimType_>
+inline static Eigen::Matrix<PrimType_, 3, 3> skewVector(
+    const Eigen::Matrix<PrimType_, 3, 1>& vec) {
   Eigen::Matrix<PrimType_, 3, 3> mat;
   mat << 0, -vec(2), vec(1), vec(2), 0, -vec(0), -vec(1), vec(0), 0;
   return mat;
 }
 
-template<typename DERIVED, typename GET, unsigned int D, unsigned int E = D>
-class ElementBase{
+template <typename DERIVED, typename GET, unsigned int D, unsigned int E = D>
+class ElementBase {
  public:
   EIGEN_MAKE_ALIGNED_OPERATOR_NEW
   ElementBase(){};
   virtual ~ElementBase(){};
   static const unsigned int D_ = D;
   static const unsigned int E_ = E;
-  typedef Eigen::Matrix<double,D_,1> mtDifVec;
+  typedef Eigen::Matrix<double, D_, 1> mtDifVec;
   typedef GET mtGet;
   std::string name_;
   virtual void boxPlus(const mtDifVec& vecIn, DERIVED& stateOut) const = 0;
@@ -39,12 +40,12 @@ class ElementBase{
   virtual void setIdentity() = 0;
   virtual void setRandom(unsigned int& s) = 0;
   virtual void fix() = 0;
-  static DERIVED Identity(){
+  static DERIVED Identity() {
     DERIVED identity;
     identity.setIdentity();
     return identity;
   }
-  DERIVED& operator=(DERIVED other){
+  DERIVED& operator=(DERIVED other) {
     other.swap(*this);
     return *this;
   }
@@ -52,116 +53,117 @@ class ElementBase{
   virtual const mtGet& get(unsigned int i) const = 0;
 };
 
-class NormalVectorElement: public ElementBase<NormalVectorElement,NormalVectorElement,2> {
+class NormalVectorElement
+    : public ElementBase<NormalVectorElement, NormalVectorElement, 2> {
  public:
   EIGEN_MAKE_ALIGNED_OPERATOR_NEW
   Eigen::Quaterniond q_;
   const Eigen::Vector3d e_x;
   const Eigen::Vector3d e_y;
   const Eigen::Vector3d e_z;
-  NormalVectorElement(): e_x(1,0,0), e_y(0,1,0), e_z(0,0,1){}
-  NormalVectorElement(const NormalVectorElement& other): e_x(1,0,0), e_y(0,1,0), e_z(0,0,1){
+  NormalVectorElement() : e_x(1, 0, 0), e_y(0, 1, 0), e_z(0, 0, 1) {}
+  NormalVectorElement(const NormalVectorElement& other)
+      : e_x(1, 0, 0), e_y(0, 1, 0), e_z(0, 0, 1) {
     q_ = other.q_;
   }
-  NormalVectorElement(const Eigen::Vector3d& vec): e_x(1,0,0), e_y(0,1,0), e_z(0,0,1){
+  NormalVectorElement(const Eigen::Vector3d& vec)
+      : e_x(1, 0, 0), e_y(0, 1, 0), e_z(0, 0, 1) {
     setFromVector(vec);
   }
-  NormalVectorElement(const Eigen::Quaterniond& q): e_x(1,0,0), e_y(0,1,0), e_z(0,0,1){
+  NormalVectorElement(const Eigen::Quaterniond& q)
+      : e_x(1, 0, 0), e_y(0, 1, 0), e_z(0, 0, 1) {
     q_ = q;
   }
   virtual ~NormalVectorElement(){};
-  Eigen::Vector3d getVec() const{
-    return q_ * e_z;
-  }
-  Eigen::Vector3d getPerp1() const{
-    return q_ * e_x;
-  }
-  Eigen::Vector3d getPerp2() const{
-    return q_ * e_y;
-  }
-  NormalVectorElement& operator=(const NormalVectorElement& other){
+  Eigen::Vector3d getVec() const { return q_ * e_z; }
+  Eigen::Vector3d getPerp1() const { return q_ * e_x; }
+  Eigen::Vector3d getPerp2() const { return q_ * e_y; }
+  NormalVectorElement& operator=(const NormalVectorElement& other) {
     q_ = other.q_;
     return *this;
   }
   static Eigen::Vector3d getRotationFromTwoNormals(
-      const Eigen::Vector3d& a, const Eigen::Vector3d& b, const Eigen::Vector3d& a_perp){
+      const Eigen::Vector3d& a, const Eigen::Vector3d& b,
+      const Eigen::Vector3d& a_perp) {
     const Eigen::Vector3d cross = a.cross(b);
     const double crossNorm = cross.norm();
     const double c = a.dot(b);
     const double angle = std::acos(c);
-    if(crossNorm<1e-6){
-      if(c>0){
+    if (crossNorm < 1e-6) {
+      if (c > 0) {
         return cross;
       } else {
-        return a_perp*M_PI;
+        return a_perp * M_PI;
       }
     } else {
-      return cross*(angle/crossNorm);
+      return cross * (angle / crossNorm);
     }
   }
   static Eigen::Vector3d getRotationFromTwoNormals(
-      const NormalVectorElement& a, const NormalVectorElement& b){
-    return getRotationFromTwoNormals(a.getVec(),b.getVec(),a.getPerp1());
+      const NormalVectorElement& a, const NormalVectorElement& b) {
+    return getRotationFromTwoNormals(a.getVec(), b.getVec(), a.getPerp1());
   }
   static Eigen::Matrix3d getRotationFromTwoNormalsJac(
-      const Eigen::Vector3d& a, const Eigen::Vector3d& b){
+      const Eigen::Vector3d& a, const Eigen::Vector3d& b) {
     const Eigen::Vector3d cross = a.cross(b);
     const double crossNorm = cross.norm();
-    Eigen::Vector3d crossNormalized = cross/crossNorm;
+    Eigen::Vector3d crossNormalized = cross / crossNorm;
     Eigen::Matrix3d crossNormalizedSqew = skewVector(crossNormalized);
     const double c = a.dot(b);
     const double angle = std::acos(c);
-    if(crossNorm<1e-6){
-      if(c>0){
+    if (crossNorm < 1e-6) {
+      if (c > 0) {
         return -skewVector(b);
       } else {
         return Eigen::Matrix3d::Zero();
       }
     } else {
-      return -1/crossNorm*(crossNormalized*b.transpose()-(crossNormalizedSqew*crossNormalizedSqew*skewVector(b)*angle));
+      return -1 / crossNorm *
+             (crossNormalized * b.transpose() -
+              (crossNormalizedSqew * crossNormalizedSqew * skewVector(b) *
+               angle));
     }
   }
-  static Eigen::Matrix3d getRotationFromTwoNormalsJac(const NormalVectorElement& a, const NormalVectorElement& b){
-    return getRotationFromTwoNormalsJac(a.getVec(),b.getVec());
+  static Eigen::Matrix3d getRotationFromTwoNormalsJac(
+      const NormalVectorElement& a, const NormalVectorElement& b) {
+    return getRotationFromTwoNormalsJac(a.getVec(), b.getVec());
   }
-  void setFromVector(Eigen::Vector3d vec){
+  void setFromVector(Eigen::Vector3d vec) {
     const double d = vec.norm();
-    if(d > 1e-6){
-      vec = vec/d;
-      Eigen::Vector3d rv = getRotationFromTwoNormals(e_z,vec,e_x);
+    if (d > 1e-6) {
+      vec = vec / d;
+      Eigen::Vector3d rv = getRotationFromTwoNormals(e_z, vec, e_x);
       Eigen::AngleAxisd aa(rv.norm(), rv.normalized());
       q_ = Eigen::Quaterniond(aa);
     } else {
       q_.setIdentity();
     }
   }
-  NormalVectorElement rotated(const QPD& q) const{
-    return NormalVectorElement(q*q_);
+  NormalVectorElement rotated(const QPD& q) const {
+    return NormalVectorElement(q * q_);
   }
-  NormalVectorElement inverted() const{
+  NormalVectorElement inverted() const {
     Eigen::Quaterniond q(Eigen::AngleAxisd(M_PI, getPerp1()));
-    return NormalVectorElement(q*q_);
+    return NormalVectorElement(q * q_);
   }
-  void boxPlus(const mtDifVec& vecIn, NormalVectorElement& stateOut) const{
-    Eigen::Vector3d Nu = vecIn(0)*getPerp1()+vecIn(1)*getPerp2();
+  void boxPlus(const mtDifVec& vecIn, NormalVectorElement& stateOut) const {
+    Eigen::Vector3d Nu = vecIn(0) * getPerp1() + vecIn(1) * getPerp2();
     Eigen::Quaterniond q(Eigen::AngleAxisd(Nu.norm(), Nu.normalized()));
-    stateOut.q_ = q*q_;
+    stateOut.q_ = q * q_;
   }
-  void boxMinus(const NormalVectorElement& stateIn, mtDifVec& vecOut) const{
-    vecOut = stateIn.getN().transpose()*getRotationFromTwoNormals(stateIn,*this);
+  void boxMinus(const NormalVectorElement& stateIn, mtDifVec& vecOut) const {
+    vecOut =
+        stateIn.getN().transpose() * getRotationFromTwoNormals(stateIn, *this);
   }
-  void boxMinusJac(const NormalVectorElement& stateIn, MXD& matOut) const{
-    matOut = -stateIn.getN().transpose()*getRotationFromTwoNormalsJac(*this,stateIn)*this->getM();
+  void boxMinusJac(const NormalVectorElement& stateIn, MXD& matOut) const {
+    matOut = -stateIn.getN().transpose() *
+             getRotationFromTwoNormalsJac(*this, stateIn) * this->getM();
   }
-  void print() const{
-    std::cout << getVec().transpose() << std::endl;
-  }
-  void setIdentity(){
-    q_.setIdentity();
-  }
-  void setRandom(unsigned int& s){
-    std::default_random_engine generator (s);
-    std::normal_distribution<double> distribution (0.0,1.0);
+  void print() const { std::cout << getVec().transpose() << std::endl; }
+  void setIdentity() { q_.setIdentity(); }
+  void setRandom(unsigned int& s) {
+    std::default_random_engine generator(s);
+    std::normal_distribution<double> distribution(0.0, 1.0);
     q_.w() = distribution(generator);
     q_.x() = distribution(generator);
     q_.y() = distribution(generator);
@@ -169,152 +171,135 @@ class NormalVectorElement: public ElementBase<NormalVectorElement,NormalVectorEl
     q_.normalize();
     s++;
   }
-  void fix(){
-    q_.normalize();
-  }
-  mtGet& get(unsigned int i = 0){
-    assert(i==0);
+  void fix() { q_.normalize(); }
+  mtGet& get(unsigned int i = 0) {
+    assert(i == 0);
     return *this;
   }
-  const mtGet& get(unsigned int i = 0) const{
-    assert(i==0);
+  const mtGet& get(unsigned int i = 0) const {
+    assert(i == 0);
     return *this;
   }
-  Eigen::Matrix<double,3,2> getM() const {
-    Eigen::Matrix<double,3,2> M;
+  Eigen::Matrix<double, 3, 2> getM() const {
+    Eigen::Matrix<double, 3, 2> M;
     M.col(0) = -getPerp2();
     M.col(1) = getPerp1();
     return M;
   }
-  Eigen::Matrix<double,3,2> getN() const {
-    Eigen::Matrix<double,3,2> M;
+  Eigen::Matrix<double, 3, 2> getN() const {
+    Eigen::Matrix<double, 3, 2> M;
     M.col(0) = getPerp1();
     M.col(1) = getPerp2();
     return M;
   }
-  const double* data() const {
-      return q_.coeffs().data();
-  }
+  const double* data() const { return q_.coeffs().data(); }
 };
 
-class AngleElement: ElementBase<AngleElement, AngleElement, 1>{
-public:
-    EIGEN_MAKE_ALIGNED_OPERATOR_NEW
+// Angle range [0, \pi]
+class AngleElement : ElementBase<AngleElement, AngleElement, 1> {
+ public:
+  EIGEN_MAKE_ALIGNED_OPERATOR_NEW
 
-    void setFromCosine(double cosTheta) {
-        cs_[0] = cosTheta;
-        cs_[1] = std::sqrt(1 - cosTheta * cosTheta);
-    }
+  void setFromCosine(double cosTheta) {
+    cs_[0] = cosTheta;
+    cs_[1] = std::sqrt(1 - cosTheta * cosTheta);
+  }
 
-    void set(const double* parameters) {
-        cs_[0] = parameters[0];
-        cs_[1] = parameters[1];
-    }
+  void set(const double* parameters) {
+    cs_[0] = parameters[0];
+    cs_[1] = parameters[1];
+  }
 
-    AngleElement() {}
+  AngleElement() {}
 
-    AngleElement(double cosTheta) {
-        setFromCosine(cosTheta);
-    }
+  AngleElement(double cosTheta) { setFromCosine(cosTheta); }
 
-    virtual void boxPlus(const mtDifVec& vecIn, AngleElement& stateOut) const {
-      double cd = std::cos(vecIn[0]);
-      double sd = std::sin(vecIn[0]);
-      double cSum = cs_[0] * cd - cs_[1] * sd;
-      double sSum = cs_[1] * cd + sd * cs_[0];
-      stateOut.cs_[0] = cSum;
-      stateOut.cs_[1] = sSum;
-    }
+  virtual void boxPlus(const mtDifVec& vecIn, AngleElement& stateOut) const {
+    double cd = std::cos(vecIn[0]);
+    double sd = std::sin(vecIn[0]);
+    double cSum = cs_[0] * cd - cs_[1] * sd;
+    double sSum = cs_[1] * cd + sd * cs_[0];
+    stateOut.cs_[0] = cSum;
+    stateOut.cs_[1] = sSum;
+  }
 
-    virtual void boxMinus(const AngleElement& stateIn, mtDifVec& vecOut) const {
+  virtual void boxMinus(const AngleElement& stateIn, mtDifVec& vecOut) const {
+    double cSum = cs_[0] * stateIn.data()[0] + cs_[1] * stateIn.data()[1];
+    double sSum = cs_[1] * stateIn.data()[0] - cs_[0] * stateIn.data()[1];
 
-    }
-    virtual void boxMinusJac(const AngleElement& stateIn, MXD& matOut) const {
+    vecOut[0] = std::atan2(sSum, cSum);
+  }
 
-    }
-    virtual void print() const {
+  virtual void boxMinusJac(const AngleElement& /*stateIn*/, MXD& matOut) const {
+    matOut = Eigen::Matrix<double, 1, 1>(1);
+  }
 
-    }
-    virtual void setIdentity() {
-      cs_[0] = 1;
-      cs_[1] = 0;
-    }
-    virtual void setRandom(unsigned int& s) {
-        std::default_random_engine generator (s);
-        std::normal_distribution<double> distribution (0.0,1.0);
-        cs_[0] = distribution(generator);
-        cs_[1] = distribution(generator);
-        cs_.normalize();
-        s++;
-    }
-    virtual void fix() {
+  virtual void print() const {
+    std::cout << getAngle() << std::endl;
+  }
+  virtual void setIdentity() {
+    cs_[0] = 1;
+    cs_[1] = 0;
+  }
+  virtual void setRandom(unsigned int& s) {
+    std::default_random_engine generator(s);
+    std::normal_distribution<double> distribution(0.0, 1.0);
+    cs_[0] = distribution(generator);
+    cs_[1] = distribution(generator);
+    cs_.normalize();
+    s++;
+  }
+  virtual void fix() { cs_.normalize(); }
 
-    }
+  virtual mtGet& get(unsigned int /*i*/ = 0) { return *this; }
 
-    virtual mtGet& get(unsigned int i = 0) {
-      return *this;
-    }
+  virtual const mtGet& get(unsigned int /*i*/ = 0) const { return *this; }
+  const double* data() const { return cs_.data(); }
+  double getAngle() const { return std::acos(cs_[0]); }
 
-    virtual const mtGet& get(unsigned int i = 0) const {
-      return *this;
-    }
-    const double* data() const {
-        return cs_.data();
-    }
-    const double getAngle() const {
-       return std::acos(cs_[0]);
-    }
-private:
-    Eigen::Vector2d cs_; // cos(\theta) sin(\theta). \theta is in the range of [0, \pi).
+ private:
+  Eigen::Vector2d cs_;  // cos(\theta) sin(\theta). \theta is in the range of [0, \pi).
 };
 
 class ParallaxAnglePoint {
-public:
-    EIGEN_MAKE_ALIGNED_OPERATOR_NEW
-    ParallaxAnglePoint() {
+ public:
+  EIGEN_MAKE_ALIGNED_OPERATOR_NEW
+  ParallaxAnglePoint() {}
+  virtual ~ParallaxAnglePoint() {}
+  /**
+   * @brief ParallaxAnglePoint
+   * @param bearing bearing vector in main anchor frame, not necessarily has
+   * unit norm.
+   * @param cosTheta cos(\theta)
+   */
+  ParallaxAnglePoint(const Eigen::Vector3d& bearing, double cosTheta)
+      : theta_(cosTheta) {
+    n_.setFromVector(bearing);
+  }
 
-    }
+  void copy(std::vector<double>* parameters) const {
+    parameters->resize(6);
+    memcpy(parameters->data(), n_.data(), sizeof(double) * 4);
+    memcpy(parameters->data() + 4, theta_.data(), sizeof(double) * 2);
+  }
 
-    /**
-     * @brief ParallaxAnglePoint
-     * @param bearing bearing vector in main anchor frame, not necessarily has unit norm.
-     * @param cosTheta cos(\theta)
-     */
-    ParallaxAnglePoint(const Eigen::Vector3d& bearing, double cosTheta) : theta_(cosTheta) {
-        n_.setFromVector(bearing);
-    }
+  void set(const double* parameters) {
+    n_ = NormalVectorElement(Eigen::Map<const Eigen::Quaterniond>(parameters));
+    theta_.set(parameters + 4);
+  }
 
-    void copy(std::vector<double>* parameters) const {
-        parameters->resize(6);
-        memcpy(parameters->data(), n_.data(), sizeof(double) * 4);
-        memcpy(parameters->data() + 4, theta_.data(), sizeof(double) * 2);
-    }
+  virtual void setRandom(unsigned int& s) {
+    n_.setRandom(s);
+    theta_.setRandom(s);
+  }
+  Eigen::Vector3d getVec() const { return n_.getVec(); }
+  double getAngle() const { return theta_.getAngle(); }
+  double cosTheta() const { return theta_.data()[0]; }
+  double sinTheta() const { return theta_.data()[1]; }
 
-    void set(const double* parameters) {
-        n_ = NormalVectorElement(Eigen::Map<const Eigen::Quaterniond>(parameters));
-        theta_.set(parameters + 4);
-    }
-
-    virtual void setRandom(unsigned int& s) {
-        n_.setRandom(s);
-        theta_.setRandom(s);
-    }
-    Eigen::Vector3d getVec() const {
-      return n_.getVec();
-    }
-    double getAngle() const {
-      return theta_.getAngle();
-    }
-    double cosTheta() const {
-      return theta_.data()[0];
-    }
-    double sinTheta() const {
-      return theta_.data()[1];
-    }
-
-    NormalVectorElement n_;
-    AngleElement theta_;
+  NormalVectorElement n_;
+  AngleElement theta_;
 };
 
-} // namespace LWF
-#endif // INCLUDE_MSCKF_PARALLAX_ANGLE_POINT_HPP_
+}  // namespace LWF
+#endif  // INCLUDE_MSCKF_PARALLAX_ANGLE_POINT_HPP_
--- okvis_ceres/test/TestParallaxAnglePoint.cpp
+++ okvis_ceres/test/TestParallaxAnglePoint.cpp
@@ -41,3 +41,57 @@ TEST_F(ParallaxAnglePointTest, GetBearing) {
 TEST_F(ParallaxAnglePointTest, GetTheta) {
   EXPECT_LT(std::fabs(pap_.getAngle() - vecAndTheta_[3]), eps);
 }
+
+
+
+class AngleElementTest : public ::testing::Test {
+ protected:
+  AngleElementTest() {}
+  void SetUp() override {
+    theta_ = Eigen::Matrix<double, 2, 1>::Random();
+    theta_ = theta_.cwiseAbs();
+    ct_ << std::cos(theta_[0]), std::cos(theta_[1]);
+    angle0_ = LWF::AngleElement(ct_[0]);
+    angle1_ = LWF::AngleElement(ct_[1]);
+  }
+
+  Eigen::Matrix<double, 2, 1> theta_;
+  Eigen::Matrix<double, 2, 1> ct_;
+  LWF::AngleElement angle0_;
+  LWF::AngleElement angle1_;
+};
+
+TEST_F(AngleElementTest, assignOperator) {
+  LWF::AngleElement angle2(ct_[1]);
+  LWF::AngleElement angle3 = angle2;
+  EXPECT_NEAR(angle2.getAngle(), theta_[1], 1e-7);
+  EXPECT_NEAR(angle1_.getAngle(), theta_[1], 1e-7);
+  EXPECT_NEAR(angle3.getAngle(), theta_[1], 1e-7);
+}
+
+TEST_F(AngleElementTest, boxMinus) {
+  Eigen::Matrix<double, 1, 1> delta;
+  angle1_.boxMinus(angle0_, delta);
+  EXPECT_NEAR(delta[0], theta_[1] - theta_[0], 1e-7);
+}
+
+TEST_F(AngleElementTest, boxMinus2) {
+  Eigen::Matrix<double, 1, 1> delta;
+  angle1_.boxMinus(angle0_, delta);
+  LWF::AngleElement angle2 = angle0_;
+  angle2.boxPlus(delta, angle2);
+  EXPECT_NEAR(angle2.getAngle(), angle1_.getAngle(), 1e-7);
+}
+
+TEST_F(AngleElementTest, boxMinusJac) {
+  Eigen::Matrix<double, 1, 1> delta;
+  angle1_.boxMinus(angle0_, delta);
+  Eigen::MatrixXd jac;
+  angle1_.boxMinusJac(angle0_, jac);
+  double h = 0.00001;
+  LWF::AngleElement angle2(std::cos(theta_[1] + h));
+  Eigen::Matrix<double, 1, 1> deltah;
+  angle2.boxMinus(angle0_, deltah);
+
+  EXPECT_NEAR(jac(0, 0), (deltah(0, 0) - delta(0, 0)) / h, 1e-7);
+}
